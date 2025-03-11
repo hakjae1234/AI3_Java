41840f4704ea99436391f1848971ee6ad9dc50a7
TJ-BU-706-P18 <TJ-BU-706-P18@DESKTOP-BON3M82>
2025-03-11 17:47:07 +0900

    2일차

---
+++ Java/src/Day01/Ex01_Print.java
@@ -0,0 +1,17 @@
+package Day01;
+
+public class Ex01_Print {
+	
+	// main : ctrl + space
+	// 프로그램이 시작되는 메소드
+	public static void main(String[] args) {
+		// 기본 출력 메소드
+		// * print(), println() 메소드를 호출하여 출력한다.
+		// * System.out 출력 객체를 사용한다.
+		System.out.println("안녕하세요");
+		System.out.println("안녕하세요 출력 후 줄 바꿈");
+		System.out.println("첫 수업");
+		
+		
+	}
+}
---
+++ Java/src/Day02/EX05_Long.java
@@ -0,0 +1,23 @@
+package Day02;
+
+public class EX05_Long {
+
+	public static void main(String[] args) {
+		// int (4bytes : 32bits) : 2^32개 : 약 42억개
+		// int 수 표현 범위 		 : -21억xxx ~ 21억xxx
+		
+		// long (8bytes : 64bits) : 2^64 개
+		long ln1 = 1000;
+		long ln2 = 2100000000;
+		long ln3 = 2200000000L;
+		// L을 붙이지 않으면 기본정수는 int 타입으로 인식되어 21억을 표현할 수 없다.
+		
+		System.out.println("ln1 : " + ln1);
+		System.out.println("ln2 : " + ln2);
+		System.out.println("ln3 : " + ln3);
+		
+		
+		
+		
+	}
+}
